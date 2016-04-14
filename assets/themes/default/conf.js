5427f9385a4632bd2ccbef3a65dbbf2019c44333
Patrick Champclos <patrick.champclos@veo-labs.com>
2016-04-14 11:50:14 +0200

    Add color palette in conf

--- assets/themes/default/conf.js
+++ assets/themes/default/conf.js
@@ -1,8 +1,19 @@
 'use strict';
 
-angular.module('ov.configuration', [], ['$provide', function($provide) {
+var app = angular.module('ov.configuration', [],
+
+// Set Links
+ ['$provide', function($provide) {
   $provide.value('links', {
     contactMailTo: 'mailto:coucou@coucou.com',
     helpUrl: 'http://www.google.fr'
   });
+}]);
+
+// Set Themes
+app.config(['$mdThemingProvider', function($mdThemingProvider) {
+	$mdThemingProvider.theme('default')
+	.primaryPalette('blue')
+	.accentPalette('grey')
+	.warnPalette('red');
 }]);
