3cc0f7377dd0aa6d3f2824ef16f53b65968b9e0a
Patrick Champclos <patrick.champclos@veo-labs.com>
2017-01-04 11:50:00 +0100

    Auto-generated documentation for version 1.1.1

---
+++ 1.1.1/api/client/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "TranslateFilter",
+        "applicationService"
+    ],
+    "modules": [
+        "ov",
+        "ov.i18n",
+        "ov.portal"
+    ],
+    "allModules": [
+        {
+            "displayName": "ov",
+            "name": "ov",
+            "description": "Defines service to manage the video search."
+        },
+        {
+            "displayName": "ov.i18n",
+            "name": "ov.i18n",
+            "description": "Control internationalization."
+        },
+        {
+            "displayName": "ov.portal",
+            "name": "ov.portal",
+            "description": "Main OpenVeo Portal module.\n\nInitializes routes."
+        }
+    ],
+    "elements": []
+} };
+});
