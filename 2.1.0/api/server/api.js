65683505194a18a4b636b6412942666fd884763c
Maxime Béguin <maxime.beguin@veo-labs.com>
2017-09-18 09:02:34 +0200

    Auto-generated documentation for version 2.1.0

---
+++ 2.1.0/api/server/api.js
@@ -0,0 +1,55 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CAS",
+        "CAS1",
+        "CAS2",
+        "CAS3",
+        "CasStrategy",
+        "FilterCache",
+        "HTTP_ERRORS",
+        "Server",
+        "VideoCache",
+        "WebserviceClient",
+        "defaultController",
+        "errorController",
+        "searchController",
+        "statisticsController",
+        "strategies"
+    ],
+    "modules": [
+        "cache",
+        "controllers",
+        "passport",
+        "passport-cas",
+        "portal"
+    ],
+    "allModules": [
+        {
+            "displayName": "cache",
+            "name": "cache"
+        },
+        {
+            "displayName": "controllers",
+            "name": "controllers",
+            "description": "Controllers provide all server route actions."
+        },
+        {
+            "displayName": "passport",
+            "name": "passport",
+            "description": "Defines passport strategies."
+        },
+        {
+            "displayName": "passport-cas",
+            "name": "passport-cas",
+            "description": "Defines a passport cas strategy."
+        },
+        {
+            "displayName": "portal",
+            "name": "portal",
+            "description": "Defines Portal HTTP Server."
+        }
+    ],
+    "elements": []
+} };
+});
