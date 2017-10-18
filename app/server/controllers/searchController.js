veo-labs/openveo-portal

commit 3cf99ea5cbddca138c143685b3eada116f269e9f

Add local authentication and group associations
Local strategy has been added to be able to authenticate using a user from the OpenVeo Portal database. Consequently a super administrator is created when using the install.js script. Another script is also available to add a user to the database as there is no back end (scripts/createUser.js).
It is now also possible to specify associations between an SSO provider and OpenVeo groups. Associations can be made using server configuration file.
CAS strategy was directly implemented in the portal. It now uses strategies from OpenVeo API.

app/server/controllers/searchController.js
@@ -24,9 +24,29 @@ const videoCache = process.require('/app/server/serverCache/VideoCache');
 const filterCache = process.require('/app/server/serverCache/FilterCache');
 const VIDEO_PUBLISH_STATES = 12;
 
+/**
+ * Searches OpenVeo Publish videos.
+ *
+ * @method searchAction
+ * @static
+ * @async
+ * @param {Request} request ExpressJS HTTP Request
+ * @param {Object} request.body Request's body
+ * @param {Object} request.body.filter Web service endpoint filters
+ * @param {String} [request.body.filter.query] Filter to get only videos which contain the query
+ * inside the title or description
+ * @param {String} [request.body.filter.dateStart] Filter to get only videos after a particular date
+ * @param {String} [request.body.filter.dateEnd] Filter to get only videos before a particular date
+ * @param {String} [request.body.filter.sortBy] The name of the property to sort by (either "views" or "date")
+ * @param {String} [request.body.filter.sortOrder] The order of the sort (either "asc" or "desc")
+ * @param {Object} request.body.pagination Pagination to set limit and page
+ * @param {Object} [request.body.pagination.limit] Maximum number of videos to retreive
+ * @param {Object} [request.body.pagination.page] The number of the page to retrieve
+ * @param {Response} response ExpressJS HTTP Response
+ * @param {Function} next Function to defer execution to the next registered middleware
+ */
 module.exports.searchAction = (request, response, next) => {
   const body = request.body || {};
-  const orderedProperties = ['views', 'date'];
   const openVeoClient = webserviceClient.get();
   let params;
   let paginate;
@@ -37,8 +57,7 @@ module.exports.searchAction = (request, response, next) => {
       query: {type: 'string'},
       dateStart: {type: 'string'},
       dateEnd: {type: 'string'},
-      categories: {type: 'array<string>'},
-      sortBy: {type: 'string', in: orderedProperties, default: 'date'},
+      sortBy: {type: 'string', in: ['views', 'date'], default: 'date'},
       sortOrder: {type: 'string', in: ['asc', 'desc'], default: 'desc'}
     });
   } catch (error) {
@@ -50,24 +69,30 @@ module.exports.searchAction = (request, response, next) => {
   }
 
   // Public group filter not defined
-  if (!portalConf.conf.publicFilter.length || portalConf.conf.publicFilter[0] == '') {
+  if (!portalConf.conf.publicFilter.length) {
     process.logger.error(errors.CONF_ERROR.message, {error: errors.CONF_ERROR, method: 'searchAction'});
     return next(errors.CONF_ERROR);
   }
 
   // Add group filter
+  if (!request.isAuthenticated()) {
 
-  // Add public groups
-  params['groups'] = portalConf.conf.publicFilter || [];
+    // Anonymous user (not authenticated)
+    // Add public groups
+    params['groups'] = portalConf.conf.publicFilter || [];
 
-  if (request.isAuthenticated()) {
+  } else if (request.user.id !== portalConf.superAdminId) {
+
+    // User authenticated and not the super administrator
+    // Add public groups
+    params['groups'] = portalConf.conf.publicFilter || [];
 
     // Add private groups
     if (portalConf.conf.privateFilter)
       params['groups'] = params['groups'].concat(portalConf.conf.privateFilter);
 
     // Add user groups
-    if (request.user.groups.length)
+    if (request.user.groups && request.user.groups.length)
       params['groups'] = params['groups'].concat(request.user.groups);
 
   }
@@ -158,23 +183,33 @@ module.exports.getCategoriesAction = (request, response, next) => {
   });
 };
 
+/**
+ * Searches for an OpenVeo Publish video.
+ *
+ * @method getVideoAction
+ * @static
+ * @async
+ * @param {Request} request ExpressJS HTTP Request
+ * @param {Object} request.params Request's parameters
+ * @param {Object} request.params.id The id of the video to fetch
+ * @param {Response} response ExpressJS HTTP Response
+ * @param {Function} next Function to defer execution to the next registered middleware
+ */
 module.exports.getVideoAction = (request, response, next) => {
   const videoCacheInstance = videoCache.getVideoCache();
-  videoCacheInstance.getVideo(request.params.id, (error, res) => {
-    if (error) {
-      next(error);
-      return;
-    }
+
+  videoCacheInstance.getVideo(request.params.id, (error, video) => {
+    if (error) return next(error);
 
     const isInPublicGroups = openVeoApi.util.intersectArray(
-      res.entity.metadata.groups,
+      video.entity.metadata.groups,
       portalConf.conf.publicFilter
     ).length;
 
     if (isInPublicGroups) {
 
       // Video is public
-      response.send(res);
+      response.send(video);
 
     } else if (!request.isAuthenticated()) {
 
@@ -185,17 +220,21 @@ module.exports.getVideoAction = (request, response, next) => {
     } else {
 
       // User is authenticated
+
+      // Super administrator can see all videos
+      if (request.user.id === portalConf.superAdminId)
+        return response.send(video);
+
       // Find out if video is part of private groups or its groups
       // If video is part of these groups user is allowed to access the video
-
       const isInPrivateGroups = openVeoApi.util.intersectArray(
-        res.entity.metadata.groups, portalConf.conf.privateFilter
+        video.entity.metadata.groups, portalConf.conf.privateFilter
       ).length;
       const isInUserGroups = openVeoApi.util.intersectArray(
-        res.entity.metadata.groups, request.user.groups
+        video.entity.metadata.groups, request.user.groups
       ).length;
 
-      if (isInPrivateGroups || isInUserGroups) response.send(res);
+      if (isInPrivateGroups || isInUserGroups) response.send(video);
       else next(errors.GET_VIDEO_NOT_ALLOWED);
     }
   });

app/client/front/js/authentication/AuthenticationApp.js
@@ -8,7 +8,7 @@
  */
 
 (function(angular) {
-  var app = angular.module('ov.authentication', ['ov.storage']);
+  var app = angular.module('ov.authentication', []);
 
   /**
    * Defines an authentication service to deal with user authentication.
@@ -16,31 +16,27 @@
    *
    * @class authenticationService
    */
-  function AuthenticationService($http, storage) {
+  function AuthenticationService($http) {
     var basePath = '/';
     var userInfo;
 
     /**
-     * Initializes user information from local storage.
+     * Initializes user information.
      */
     function init() {
-      var info = storage.get('userInfo');
-      if (info)
-        userInfo = JSON.parse(info);
+      userInfo = openVeoPortalSettings.user;
     }
 
     /**
      * Signs in using the given credentials.
      *
-     * @param {String} type The type of authentication
      * @param {String} login The login
      * @param {String} password The password
      * @return {HttPromise} The authentication promise
      * @method login
      */
-    function login(type, login, password) {
+    function login(login, password) {
       return $http.post(basePath + 'authenticate', {
-        type: type,
         login: login,
         password: password
       });
@@ -73,13 +69,7 @@
      * @method setUserInfo
      */
     function setUserInfo(info) {
-      if (info) {
-        storage.set('userInfo', JSON.stringify(info));
-        userInfo = info;
-      } else {
-        userInfo = null;
-        storage.remove('userInfo');
-      }
+      userInfo = (info) ? info : null;
     }
 
     init();
@@ -94,6 +84,6 @@
   }
 
   app.factory('authenticationService', AuthenticationService);
-  AuthenticationService.$inject = ['$http', 'storage'];
+  AuthenticationService.$inject = ['$http'];
 
 })(angular);

app/client/front/js/ov/MainController.js
@@ -50,6 +50,7 @@
   $location, $filter, $window, $analytics, searchService, authenticationService) {
     var self = this;
     var urlParams = $location.search();
+    var authenticationStrategies = openVeoPortalSettings.authenticationStrategies;
     this.currentNavItem = null;
     this.isLoggingOut = false;
     $scope.context = {keepContext: false};
@@ -231,10 +232,11 @@
      */
     this.logout = function() {
       var self = this;
+
       if ($scope.user) {
         $analytics.eventTrack('Logout');
 
-        if ($scope.user.strategy === 'cas') {
+        if ($scope.user.origin === authenticationStrategies.CAS) {
 
           // CAS strategy needs a redirection
           $window.location.href = '/logout';

app/client/front/js/ov/loginFormController.js
@@ -7,10 +7,12 @@
    */
   function LoginFormController($scope, $location, $window, $analytics, authenticationService) {
     var self = this;
+    var authenticationStrategies = openVeoPortalSettings.authenticationStrategies;
     this.theme = openVeoPortalSettings.theme;
-    this.hasCas = openVeoPortalSettings.authenticationMechanisms.indexOf('cas') >= 0;
-    this.hasLdap = openVeoPortalSettings.authenticationMechanisms.indexOf('ldapauth') >= 0;
+    this.hasCas = openVeoPortalSettings.authenticationMechanisms.indexOf(authenticationStrategies.CAS) >= 0;
     this.hasExternal = this.hasCas;
+    this.hasInternal = openVeoPortalSettings.authenticationMechanisms.indexOf(authenticationStrategies.LDAP) >= 0 ||
+      openVeoPortalSettings.authenticationMechanisms.indexOf(authenticationStrategies.LOCAL) >= 0;
     this.hasError = false;
     this.isLogging = false;
 
@@ -23,7 +25,7 @@
         this.isLogging = true;
         $analytics.eventTrack('Login');
 
-        authenticationService.login('ldapauth', this.login, this.password).then(function(result) {
+        authenticationService.login(this.login, this.password).then(function(result) {
           authenticationService.setUserInfo(result.data);
           self.isLogging = false;
           self.hasError = false;

app/client/front/views/root.html
@@ -20,6 +20,7 @@
       var openVeoPortalSettings = {
         user: [[& user]],
         authenticationMechanisms: [[& authenticationMechanisms]],
+        authenticationStrategies: [[& authenticationStrategies]],
         theme: '[[theme]]',
         useDialog: [[useDialog]]
       };

app/server/Server.js
@@ -24,10 +24,12 @@ const errorController = process.require('app/server/controllers/errorController.
 const searchController = process.require('app/server/controllers/searchController.js');
 const statisticsController = process.require('app/server/controllers/statisticsController.js');
 const authenticationController = process.require('app/server/controllers/authenticationController.js');
-const passportStrategies = process.require('app/server/passport/strategies.js');
 const portalConf = process.require('app/server/conf.js');
+const authenticator = process.require('app/server/authenticator.js');
 const configurationDirectoryPath = path.join(openVeoApi.fileSystem.getConfDir(), 'portal');
 const webservicesConf = require(path.join(configurationDirectoryPath, 'webservicesConf.json'));
+const UserProvider = process.require('app/server/providers/UserProvider.js');
+const strategyFactory = openVeoApi.passport.strategyFactory;
 
 const OPENVEO_URL = webservicesConf.path;
 
@@ -39,18 +41,88 @@ const staticServerOptions = {
   }
 };
 
+/**
+ * Initializes passport strategies to manage user authentication.
+ *
+ * @method initializePassport
+ * @private
+ */
+function initializePassport() {
+  if (!this.configuration.auth) return;
+
+  this.app.use(passport.initialize());
+  this.app.use(passport.session());
+
+  // Instantiate passport strategies
+  Object.keys(this.configuration.auth).forEach((strategy) => {
+    let strategyConf = this.configuration.auth[strategy];
+    if (Object.prototype.toString.call(strategyConf) !== '[object Object]')
+      strategyConf = {};
+
+    strategyConf.usernameField = 'login';
+    strategyConf.passwordField = 'password';
+    strategyConf.logoutUri = 'be';
+
+    if (strategy === openVeoApi.passport.STRATEGIES.LOCAL) {
+
+      // Local strategy does not work like the other strategies, the passport verify callback is used to
+      // retrieve the user instead of verifying it
+      passport.use(strategyFactory.get(
+        openVeoApi.passport.STRATEGIES.LOCAL,
+        strategyConf,
+        (login, password, callback) => {
+          authenticator.verifyUserByCredentials(login, password, (error, user) => {
+            if (error) {
+              process.logger.error(error.message, {error: error, method: 'verify'});
+              callback(null, false);
+            } else
+              callback(null, user);
+          });
+        }
+      ));
+
+    } else {
+      passport.use(strategyFactory.get(strategy, strategyConf, (user, callback) => {
+        authenticator.verifyUserAuthentication(user, strategy, (error, verifiedUser) => {
+          if (error) {
+            process.logger.error(error.message, {error: error, method: 'verify'});
+            callback(null, false);
+          } else
+            callback(null, verifiedUser);
+        });
+      }));
+    }
+
+  });
+
+  // In order to support login sessions, Passport serializes and
+  // deserializes user instances to and from the session
+  passport.serializeUser(authenticator.serializeUser);
+
+  // When subsequent requests are received, the serialized datas are used to find
+  // the user, which will be restored to req.user
+  passport.deserializeUser((id, callback) => {
+    authenticator.deserializeUser(id, (error, user) => {
+      if (error) {
+        process.logger.error(error.message, {error: error, method: 'deserializeUser'});
+        callback(null, false);
+      } else
+        callback(null, user);
+    });
+  });
+}
+
 class Server {
 
   /**
    * Creates a new Portal HTTP server.
    *
    * @example
-   *     var Server = process.require('app/server/Server.js');
-   *     var serv = new Server({
+   *     const Server = process.require('app/server/Server.js');
+   *     const serv = new Server({
    *        port: 3003,
    *        sessionSecret: '123456789',
    *        auth: {
-   *          type: 'cas',
    *          cas: {
    *            ...
    *          }
@@ -96,8 +168,10 @@ class Server {
    *
    * @method onDatabaseAvailable
    * @param {Database} database Project's database
+   * @param {Function} callback Function to call when its done with:
+   *  - **Error** An error if something went wrong
    */
-  onDatabaseAvailable(database) {
+  onDatabaseAvailable(database, callback) {
 
     // Remove x-powered-by http header
     this.app.set('x-powered-by', false);
@@ -135,30 +209,8 @@ class Server {
 
     this.app.use(session(sessionOptions));
 
-    // Authentication is activated
-    if (this.configuration.auth) {
-
-      // Initialize passport
-      this.app.use(passport.initialize());
-      this.app.use(passport.session());
-
-      // Initialize passport strategy depending on the configuration
-      const auth = this.configuration.auth;
-
-      // Load passport strategies
-      for (var type in auth)
-        passport.use(passportStrategies.get(type, auth[type]));
-
-      // Serialize user into passport session
-      passport.serializeUser((user, done) => {
-        done(null, user);
-      });
-
-      // Deserialize user from passport session
-      passport.deserializeUser((user, done) => {
-        done(null, user);
-      });
-    }
+    // Initialize authentication mechanisms
+    initializePassport.call(this);
 
     // for thumbnail only, set server as proxy to webservice server
     // path : /*/*.jpg, /*/*.jpeg, /*/*.jpg?thumb=small, /*/*.jpeg?thumb=small
@@ -239,6 +291,7 @@ class Server {
     this.app.use(openVeoApi.middlewares.logRequestMiddleware);
 
     this.mountRoutes();
+    this.createIndexes(database, callback);
   }
 
   /**
@@ -269,6 +322,19 @@ class Server {
 
     // Handle errors
     this.app.use(errorController.errorAction);
+  }
+
+  /**
+   * Creates database indexes.
+   *
+   * @method createIndexes
+   * @param {Database} database The database instance
+   * @param {Function} callback Function to call when its done with:
+   *  - **Error** An error if something went wrong
+   */
+  createIndexes(database, callback) {
+    const userProvider = new UserProvider(database);
+    userProvider.createIndexes(callback);
   }
 
   /**

app/server/authenticator.js
@@ -1,0 +1,197 @@
+'use strict';
+
+/**
+ * @module portal
+ */
+
+const async = require('async');
+const openVeoApi = require('@openveo/api');
+const UserModel = process.require('app/server/models/UserModel.js');
+const UserProvider = process.require('app/server/providers/UserProvider.js');
+const conf = process.require('app/server/conf.js');
+const storage = process.require('app/server/storage.js');
+
+/**
+ * The authenticator helps manipulate users authenticated by passport strategies.
+ *
+ * Users returned by passport are not necessary OpenVeo Portal users. It could be users from a third party
+ * authentication server. The authenticator helps making sure that the authenticated user is a ready to
+ * use OpenVeo Portal user.
+ *
+ * @class authenticator
+ * @static
+ */
+
+/**
+ * Serializes only essential user information required to retrieve it later.
+ *
+ * @method serializeUser
+ * @async
+ * @static
+ * @param {Object} user The user to serialize
+ * @param {Function} callback The function to call when it's done
+ *   - **Error** The error if an error occurred, null otherwise
+ *   - **String** The serialized user information
+ */
+module.exports.serializeUser = (user, callback) => {
+  if (!user || !user.id)
+    return callback(new Error(`Could not serialize user: unknown user "${(user ? user.id : '')}"`));
+
+  callback(null, user.id);
+};
+
+/**
+ * Fetches a user from serialized data.
+ *
+ * @method deserializeUser
+ * @async
+ * @static
+ * @param {String} data Serialized data as serialized by serializeUser(), the id of the user
+ * @param {Function} callback The function to call when it's done
+ *   - **Error** The error if an error occurred, null otherwise
+ *   - **Object** The user with its permissions
+ */
+module.exports.deserializeUser = (data, callback) => {
+  const userModel = new UserModel(new UserProvider(storage.getDatabase()));
+
+  userModel.getOne(data, null, (error, user) => {
+    if (error) return callback(error);
+    if (!user) return callback(new Error(`Unkown user "${data}"`));
+    callback(null, user);
+  });
+};
+
+/**
+ * Verifies a user as returned by the passport local strategy.
+ *
+ * @method verifyUserByCredentials
+ * @async
+ * @static
+ * @param {String} email User's email
+ * @param {String} password User's password
+ * @param {Function} callback Function to call when its done
+ *  - **Error** An error if something went wrong, null otherwise
+ *  - **Object** The user with its permissions
+ */
+module.exports.verifyUserByCredentials = (email, password, callback) => {
+  const userModel = new UserModel(new UserProvider(storage.getDatabase()));
+
+  userModel.getUserByCredentials(email, password, (error, user) => {
+    if (error) return callback(error);
+    if (!user) return callback(new Error(`Email and / or password incorrect for "${email}"`));
+    callback(null, user);
+  });
+};
+
+/**
+ * Verifies user as returned by third party providers.
+ *
+ * OpenVeo Portal trusts users from third party providers, if the user does not exist in OpenVeo Portal
+ * it is created with minimum information.
+ *
+ * @method verifyUserAuthentication
+ * @async
+ * @static
+ * @param {Object} thirdPartyUser The user from the third party provider
+ * @param {String} strategy The id of the strategy
+ * @param {Function} callback Function to call when its done
+ *  - **Error** An error if something went wrong, null otherwise
+ *  - **Object** The user with its permissions
+ */
+module.exports.verifyUserAuthentication = (thirdPartyUser, strategy, callback) => {
+  const strategyConfiguration = conf.serverConf.auth[strategy];
+  const groupAssociations = strategyConfiguration.groupAssociations;
+  const thirdPartyIdAttribute = strategyConfiguration.userIdAttribute;
+  const thirdPartyNameAttribute = strategyConfiguration.userNameAttribute;
+  const thirdPartyEmailAttribute = strategyConfiguration.userEmailAttribute;
+  const thirdPartyGroupAttribute = strategyConfiguration.userGroupAttribute;
+  const userModel = new UserModel(new UserProvider(storage.getDatabase()));
+  const originId = openVeoApi.util.evaluateDeepObjectProperties(thirdPartyIdAttribute, thirdPartyUser);
+  const thirdPartyUserName = openVeoApi.util.evaluateDeepObjectProperties(thirdPartyNameAttribute, thirdPartyUser);
+  const thirdPartyUserEmail = openVeoApi.util.evaluateDeepObjectProperties(thirdPartyEmailAttribute, thirdPartyUser);
+  let originGroups = openVeoApi.util.evaluateDeepObjectProperties(thirdPartyGroupAttribute, thirdPartyUser);
+  let user;
+  let exists = false;
+  let groups = [];
+  originGroups = originGroups || [];
+  originGroups = (Array.isArray(originGroups)) ? originGroups : originGroups.split(',');
+
+  async.series([
+
+    // Test if user already exists in OpenVeo
+    (callback) => {
+      if (originId) {
+        userModel.get({
+          origin: strategy,
+          originId: originId
+        }, (error, users) => {
+          if (error) return callback(error);
+          if (users && users.length) {
+            exists = true;
+            user = users[0];
+          }
+          callback();
+        });
+      } else {
+        exists = false;
+        callback();
+      }
+    },
+
+    // Match third party user groups with OpenVeo content groups
+    (callback) => {
+      if (!originGroups) return callback();
+
+      if (groupAssociations && groupAssociations.length) {
+
+        // Look for third party user group inside OpenVeo Portal configuration
+        groupAssociations.forEach((groupAssociation) => {
+          if (originGroups.indexOf(groupAssociation.group) >= 0)
+            groups = groups.concat(groupAssociation.groups);
+        });
+      }
+      callback();
+    },
+
+    // Create user if it does not exist yet
+    (callback) => {
+      if (exists) return callback();
+
+      userModel.addThirdPartyUser({
+        name: thirdPartyUserName,
+        email: thirdPartyUserEmail,
+        groups: groups
+      }, strategy, originId, originGroups, (error, addedCount, addedUser) => {
+        if (addedUser) user = addedUser;
+        callback(error);
+      });
+    },
+
+    // Update user if information from third party provider have changed (name, email, group)
+    (callback) => {
+      if (user.name !== thirdPartyUserName ||
+          user.email !== thirdPartyUserEmail ||
+          !openVeoApi.util.areSameArrays(user.originGroups, originGroups) ||
+          !openVeoApi.util.areSameArrays(user.groups, groups)
+         ) {
+
+        user.name = thirdPartyUserName;
+        user.email = thirdPartyUserEmail;
+        user.groups = groups;
+        user.originGroups = originGroups;
+
+        userModel.updateThirdPartyUser(user.id, {
+          name: user.name,
+          email: user.email,
+          originGroups: user.originGroups,
+          groups: user.groups
+        }, strategy, callback);
+      } else
+        callback();
+    }
+
+  ], (error, results) => {
+    callback(error, user);
+  });
+
+};

app/server/conf.js
@@ -5,5 +5,6 @@ module.exports = {
   loggerConf: null,
   serverConf: null,
   databaseConf: null,
-  webservicesConf: null
+  webservicesConf: null,
+  superAdminId: '0'
 };

commit bb28c212a79ed0afcc73f54d01793d8a16f3ee69

Fix search on categories
Search on categories was broken since commit 3cf99ea (locale
authentication and group associations).

app/server/controllers/searchController.js
@@ -57,6 +57,7 @@ module.exports.searchAction = (request, response, next) => {
       query: {type: 'string'},
       dateStart: {type: 'string'},
       dateEnd: {type: 'string'},
+      categories: {type: 'array<string>'},
       sortBy: {type: 'string', in: ['views', 'date'], default: 'date'},
       sortOrder: {type: 'string', in: ['asc', 'desc'], default: 'desc'}
     });