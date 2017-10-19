1b68b0bc2ef74ec20bf03f01423f4a2c9559ae8c
Maxime Béguin <maxime.beguin@veo-labs.com>
2017-10-19 09:41:07 +0200

    Auto-generated documentation for version 3.0.0

---
+++ 3.0.0/api/server/api.js
@@ -0,0 +1,54 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "FilterCache",
+        "HTTP_ERRORS",
+        "Server",
+        "UserModel",
+        "UserProvider",
+        "VideoCache",
+        "WebserviceClient",
+        "authenticationController",
+        "authenticator",
+        "defaultController",
+        "errorController",
+        "searchController",
+        "statisticsController",
+        "storage"
+    ],
+    "modules": [
+        "cache",
+        "controllers",
+        "models",
+        "portal",
+        "providers"
+    ],
+    "allModules": [
+        {
+            "displayName": "cache",
+            "name": "cache"
+        },
+        {
+            "displayName": "controllers",
+            "name": "controllers",
+            "description": "Controllers provide all server route actions."
+        },
+        {
+            "displayName": "models",
+            "name": "models",
+            "description": "Models module includes all portal models."
+        },
+        {
+            "displayName": "portal",
+            "name": "portal",
+            "description": "Defines Portal HTTP Server."
+        },
+        {
+            "displayName": "providers",
+            "name": "providers",
+            "description": "Providers module includes all portal providers."
+        }
+    ],
+    "elements": []
+} };
+});
