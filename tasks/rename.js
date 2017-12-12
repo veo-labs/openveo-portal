5030ca50ce3de0e2eed25dfd0230a86cdc40b8cc
Maxime Béguin <maxime.beguin@veo-labs.com>
2017-12-12 17:13:44 +0100

    Fix glyphs in Grunt rename task comment

--- tasks/rename.js
+++ tasks/rename.js
@@ -4,7 +4,7 @@
 // For more information about Grunt rename, have a look at https://github.com/veo-labs/openveo-api
 module.exports = {
 
-  // Rename project's documentation directory with projet'c version
+  // Rename project documentation directory into project version
   doc: {
     src: '<%= project.root %>/site/version',
     dest: '<%= project.root %>/site/<%= pkg.version %>'
