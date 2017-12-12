5a342f12a9cbb90d8a2b51c1abb55a1451012ce6
Maxime Béguin <maxime.beguin@veo-labs.com>
2017-12-12 17:11:06 +0100

    Remove incorrect comment about Grunt rename task
    Grunt rename task is implemented in OpenVeo
    API, not in grunt-rename module.

--- tasks/rename.js
+++ tasks/rename.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Rename files or directories
-// For more information about Grunt rename, have a look at https://www.npmjs.com/package/grunt-rename
+// For more information about Grunt rename, have a look at https://github.com/veo-labs/openveo-api
 module.exports = {
 
   // Rename project's documentation directory with projet'c version
