7a2f514aad3d151d53bf450813d5cef77d3b83c0
Paul-Antoine GUYON <paul-antoine.guyon@veo-labs.com>
2018-03-22 11:54:56 +0100

    Add opaMediaSelect & opaMediaOption components
    Theese components provide a way to select a single
    media or a list of medias within a provided list
    of medias.

---
+++ app/client/admin/components/shared/mediaSelect/mediaSelect.component.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Media select component provides a (multiple or single) media selection within a list of medias
+ *
+ * <opa-media-select ng-model="{{ngModel}}"
+ *                   opa-medias="{{medias}}"
+ *                   opa-multiple="{{selectionMultiple}}">
+ * </opa-media-select>
+ *
+ * Available attributes are:
+ * - [Mixed] **ng-model**: The selected medias, it can be an array when multiple selection is active,
+ *                         or a single value (string) otherwise.
+ * - [Array] **opa-medias**: The list of available medias
+ * - [Boolean] **opa-multiple**: Enable multiple selection
+ *
+ * Requires
+ * - **ngMaterial** AngularJS Material module
+ *
+ * @module opa
+ * @class opaMediaSelect
+ */
+
+(function(app) {
+  app.component('opaMediaSelect', {
+    templateUrl: 'opa-mediaSelect.html',
+    controller: 'OpaMediaSelectController',
+    bindings: {
+      ngModel: '=',
+      opaMedias: '<',
+      opaMultiple: '<'
+    }
+  });
+
+})(angular.module('opa'));
---
+++ app/client/admin/components/shared/mediaSelect/mediaSelect.controller.js
@@ -0,0 +1,173 @@
+'use strict';
+
+/**
+ * @module opa
+ */
+
+(function(app) {
+
+  /**
+   * Manages opaSelect component.
+   *
+   * @class OpaMediaSelectController
+   * @constructor
+   */
+  function OpaMediaSelectController() {
+    var self = this;
+    var index;
+
+    Object.defineProperties(self, {
+
+      /**
+       * The list of selected medias.
+       *
+       * @property selection
+       * @type Array
+       */
+      selection: {
+        value: [],
+        writable: true
+      },
+
+      /**
+       * Defines if the model can contains one or many medias.
+       *
+       * @property multiple
+       * @type Boolean
+       */
+      multiple: {
+        value: false,
+        writable: true
+      },
+
+      /**
+       * Checkboxes states.
+       *
+       * @property checkboxesStates
+       * @type Array
+       */
+      checkboxesStates: {
+        value: [],
+        writable: true
+      },
+
+      /**
+       * Controller initialization state.
+       *
+       * The initialization of the checkboxes states can't be done before
+       * the controller initialization is completed.
+       *
+       * @property isInitialized
+       * @type Boolean
+       */
+      isInitialized: {
+        value: false,
+        writable: true
+      },
+
+      /**
+       * Initializes controller.
+       *
+       * @method $onInit
+       * @final
+       */
+      $onInit: {
+        value: function() {
+          self.multiple = self.opaMultiple || Array.isArray(self.ngModel);
+
+          if (self.ngModel)
+            self.selection = self.multiple ? self.ngModel : [self.ngModel];
+
+          self.setCheckboxesStates();
+          self.isInitialized = true;
+        }
+      },
+
+      /**
+       * Handles one-way binding properties changes.
+       *
+       * @method $onChanges
+       * @param {Object} changedProperties Properties which have changed since last digest loop
+       * @final
+       */
+      $onChanges: {
+        value: function(changedProperties) {
+          if (changedProperties.hasOwnProperty('opaMedias') && self.isInitialized) {
+            self.setCheckboxesStates();
+          }
+        }
+      },
+
+      /**
+       * Add a media to the selection.
+       *
+       * @method check
+       * @param {Object} media
+       * @final
+       */
+      check: {
+        value: function(media) {
+          index = self.opaMedias.indexOf(media);
+          if (!self.multiple) {
+            self.selection = [media.id];
+            self.ngModel = media.id;
+
+            // Uncheck all the other checkboxes
+            for (var i = 0; i < self.checkboxesStates.length; i++)
+              self.checkboxesStates[i] = i === index;
+          } else if (self.selection.indexOf(media.id) === -1) {
+            self.selection.push(media.id);
+            self.ngModel = self.selection;
+            self.checkboxesStates[index] = true;
+          }
+        }
+      },
+
+      /**
+       * Remove a media from the selection.
+       *
+       * @method uncheck
+       * @param {Object} media
+       * @final
+       */
+      uncheck: {
+        value: function(media) {
+          // Remove from selection
+          index = self.selection.indexOf(media.id);
+          if (index !== -1) {
+            self.selection.splice(index, 1);
+          }
+
+          // Disable value
+          index = self.opaMedias.indexOf(media);
+          if (index !== -1)
+            self.checkboxesStates[index] = false;
+
+          self.ngModel = self.multiple ? self.selection : '';
+        }
+      },
+
+      /**
+       * Set checkboxes states
+       *
+       * Define if checkboxes are checked or not (depending on selection)
+       * at the initialization or when the medias are changed.
+       *
+       * @method setCheckboxesStates()
+       * @final
+       */
+      setCheckboxesStates: {
+        value: function() {
+          self.checkboxesStates = [];
+          self.opaMedias.forEach(function(media) {
+            self.checkboxesStates.push(self.selection.indexOf(media.id) !== -1);
+          });
+        }
+      }
+
+    });
+  }
+
+  app.controller('OpaMediaSelectController', OpaMediaSelectController);
+
+})(angular.module('opa'));
