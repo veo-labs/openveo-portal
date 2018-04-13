f3eb7b9a95dd56a934455cce81d8a765b1438f13
Paul-Antoine GUYON <paul-antoine.guyon@veo-labs.com>
2018-04-13 17:57:07 +0200

    Manage options focus inside opaMediaSelect

--- app/client/admin/components/shared/mediaSelect/mediaSelect.controller.js
+++ app/client/admin/components/shared/mediaSelect/mediaSelect.controller.js
@@ -12,7 +12,7 @@
    * @class OpaMediaSelectController
    * @constructor
    */
-  function OpaMediaSelectController() {
+  function OpaMediaSelectController($element) {
     var self = this;
     var index;
 
@@ -48,6 +48,28 @@
        */
       checkboxesStates: {
         value: [],
+        writable: true
+      },
+
+      /**
+        * Focus states.
+       *
+       * @property focusStates
+       * @type Array
+       */
+      focusStates: {
+        value: [],
+        writable: true
+      },
+
+      /**
+       * Locks the focus when navigating between opaMediaOptions.
+       *
+       * @property keepFocus
+       * @type Boolean
+       */
+      keepFocus: {
+        value: false,
         writable: true
       },
 
@@ -94,6 +116,7 @@
         value: function(changedProperties) {
           if (changedProperties.hasOwnProperty('opaMedias') && self.isInitialized) {
             self.setCheckboxesStates();
+            self.setFocusStates();
           }
         }
       },
@@ -153,7 +176,7 @@
        * Define if checkboxes are checked or not (depending on selection)
        * at the initialization or when the medias are changed.
        *
-       * @method setCheckboxesStates()
+       * @method setCheckboxesStates
        * @final
        */
       setCheckboxesStates: {
@@ -163,11 +186,103 @@
             self.checkboxesStates.push(self.selection.indexOf(media.id) !== -1);
           });
         }
+      },
+
+      /**
+       * Set focus states
+       *
+       * Disables the focus on all medias
+       *
+       * @method setFocusStates
+       * @final
+       */
+      setFocusStates: {
+        value: function() {
+          self.focusStates = [];
+          self.opaMedias.forEach(function(media) {
+            self.focusStates.push(false);
+          });
+        }
+      },
+
+      /**
+       * Manages options focus.
+       *
+       * Enables focus on the first option when focus reaches
+       * the mediaSelect component, or switches between options
+       * when left or right arrows are pressed.
+       *
+       * @method optionFocus
+       * @final
+       */
+      optionFocus: {
+        value: function($event) {
+          if (!KeyboardEvent.prototype.isPrototypeOf($event) || !self.focusStates.length)
+            return;
+
+          if ($event.keyCode === 9) {
+            self.keepFocus = true;
+            self.focusStates[0] = true;
+            $element.children().addClass('focused');
+          } else {
+            var curIndex = self.focusStates.indexOf(true);
+            var newIndex;
+
+            if (curIndex === -1)
+              return;
+
+            switch ($event.keyCode) {
+              case 37:
+                newIndex = curIndex - 1;
+                break;
+
+              case 39:
+                newIndex = curIndex + 1;
+                break;
+
+              default:
+                return;
+            }
+
+            if (newIndex >= self.focusStates.length)
+              newIndex = 0;
+            else if (newIndex < 0)
+              newIndex = self.focusStates.length - 1;
+
+            if (newIndex === curIndex)
+              return;
+
+            self.keepFocus = true;
+            self.focusStates[curIndex] = false;
+            self.focusStates[newIndex] = true;
+          }
+        }
+      },
+
+      /**
+       * Disables options focus on blur event.
+       *
+       * @method onBlur
+       * @final
+       */
+      onBlur: {
+        value: function() {
+          if (self.keepFocus)
+            self.keepFocus = false;
+          else {
+            var index = self.focusStates.indexOf(true);
+            if (index !== -1)
+              self.focusStates[index] = false;
+
+            $element.children().removeClass('focused');
+          }
+        }
       }
 
     });
   }
 
   app.controller('OpaMediaSelectController', OpaMediaSelectController);
+  OpaMediaSelectController.$inject = ['$element'];
 
 })(angular.module('opa'));
