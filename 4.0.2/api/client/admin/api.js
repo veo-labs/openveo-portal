f41aef841af9f92c6661867c2eecdffe6115fac0
Maxime Béguin <maxime.beguin@veo-labs.com>
2018-05-14 14:18:47 +0200

    Auto-generated documentation for version 4.0.2

---
+++ 4.0.2/api/client/admin/api.js
@@ -0,0 +1,71 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "OpaAboutController",
+        "OpaAboutFactory",
+        "OpaActionsButtonController",
+        "OpaDashboardController",
+        "OpaGroupsFactory",
+        "OpaI18nFactory",
+        "OpaIndexController",
+        "OpaInfoButtonController",
+        "OpaInfoController",
+        "OpaInterceptor",
+        "OpaLicenseKeyValidatorController",
+        "OpaLiveSettingsController",
+        "OpaMediaContainerController",
+        "OpaMediaLibraryController",
+        "OpaMediaLibraryDialogController",
+        "OpaMediaOptionController",
+        "OpaMediaSelectController",
+        "OpaNavController",
+        "OpaNotificationController",
+        "OpaNotificationFactory",
+        "OpaPromotedVideosController",
+        "OpaSettingsController",
+        "OpaSettingsFactory",
+        "OpaStreamUrlValidatorController",
+        "OpaTagsController",
+        "OpaToolbarController",
+        "OpaTranslateFilter",
+        "OpaUrlFactory",
+        "OpaUserFactory",
+        "OpaVideosFactory",
+        "opa",
+        "opaAbout",
+        "opaActionsButton",
+        "opaDashboard",
+        "opaInfo",
+        "opaInfoConfiguration",
+        "opaLicenseKeyValidator",
+        "opaLiveSettings",
+        "opaMediaLibrary",
+        "opaMediaOption",
+        "opaMediaSelect",
+        "opaNav",
+        "opaNotificationConfiguration",
+        "opaPromotedVideos",
+        "opaSettings",
+        "opaStreamUrlValidator",
+        "opaTags",
+        "opaToolbar"
+    ],
+    "modules": [
+        "opa",
+        "opa.i18n"
+    ],
+    "allModules": [
+        {
+            "displayName": "opa",
+            "name": "opa",
+            "description": "OpenVeo Portal AngularJS module to manage administration interface.\n\nopa (for OpenVeo Portal Admin) module holds all components of the OpenVeo Portal administration interface.\nIt also defines opaWebServiceBasePath constant holding the base path of the Web Service."
+        },
+        {
+            "displayName": "opa.i18n",
+            "name": "opa.i18n",
+            "description": "Set of tools to handle internationalization."
+        }
+    ],
+    "elements": []
+} };
+});
