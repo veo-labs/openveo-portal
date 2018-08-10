96f655ecde5deeb88a993e20ee6dd141ea2edbd8
Maxime Béguin <maxime.beguin@veo-labs.com>
2018-08-10 15:28:45 +0200

    Add decimal value of HTTP errors to documentation
    HTTP error codes are expressed in hexadecimal and
    displayed in decimal to the user. To easily
    identify an HTTP error code communicated by a
    user, its decimal representation has been added to
    the documentation of the error.

--- app/server/httpErrors.js
+++ app/server/httpErrors.js
@@ -25,6 +25,7 @@ const HTTP_ERRORS = {
    * @property UNKNOWN_ERROR
    * @type Object
    * @final
+   * @default 1
    */
   UNKNOWN_ERROR: {
     code: 0x001,
@@ -37,6 +38,7 @@ const HTTP_ERRORS = {
    * @property CONF_ERROR
    * @type Object
    * @final
+   * @default 2
    */
   CONF_ERROR: {
     code: 0x002,
@@ -50,6 +52,7 @@ const HTTP_ERRORS = {
    * @property SEARCH_ERROR
    * @type Object
    * @final
+   * @default 3
    */
   SEARCH_ERROR: {
     code: 0x003,
@@ -62,6 +65,7 @@ const HTTP_ERRORS = {
    * @property BACK_END_AUTHENTICATION_ERROR
    * @type Object
    * @final
+   * @default 4
    */
   BACK_END_AUTHENTICATION_ERROR: {
     code: 0x004,
@@ -74,6 +78,7 @@ const HTTP_ERRORS = {
    * @property BACK_END_EXTERNAL_AUTHENTICATION_ERROR
    * @type Object
    * @final
+   * @default 5
    */
   BACK_END_EXTERNAL_AUTHENTICATION_ERROR: {
     code: 0x005,
@@ -86,6 +91,7 @@ const HTTP_ERRORS = {
    * @property CAS_AUTHENTICATION_ERROR
    * @type Object
    * @final
+   * @default 6
    */
   CAS_AUTHENTICATION_ERROR: {
     code: 0x006,
@@ -98,6 +104,7 @@ const HTTP_ERRORS = {
    * @property LDAP_AUTHENTICATION_ERROR
    * @type Object
    * @final
+   * @default 7
    */
   LDAP_AUTHENTICATION_ERROR: {
     code: 0x007,
@@ -110,6 +117,7 @@ const HTTP_ERRORS = {
    * @property GET_VIDEO_ERROR
    * @type Object
    * @final
+   * @default 8
    */
   GET_VIDEO_ERROR: {
     code: 0x008,
@@ -122,6 +130,7 @@ const HTTP_ERRORS = {
    * @property UPDATE_SETTINGS_ERROR
    * @type Object
    * @final
+   * @default 9
    */
   UPDATE_SETTINGS_ERROR: {
     code: 0x009,
@@ -134,6 +143,7 @@ const HTTP_ERRORS = {
    * @property GET_SETTING_ERROR
    * @type Object
    * @final
+   * @default 10
    */
   GET_SETTING_ERROR: {
     code: 0x00a,
@@ -146,6 +156,7 @@ const HTTP_ERRORS = {
    * @property GET_GROUPS_ERROR
    * @type Object
    * @final
+   * @default 11
    */
   GET_GROUPS_ERROR: {
     code: 0x00b,
@@ -158,6 +169,7 @@ const HTTP_ERRORS = {
    * @property DEFAULT_GET_SETTING_ERROR
    * @type Object
    * @final
+   * @default 12
    */
   DEFAULT_GET_SETTING_ERROR: {
     code: 0x00c,
@@ -170,6 +182,7 @@ const HTTP_ERRORS = {
    * @property LIVE_GET_SETTING_ERROR
    * @type Object
    * @final
+   * @default 13
    */
   LIVE_GET_SETTING_ERROR: {
     code: 0x00d,
@@ -182,6 +195,7 @@ const HTTP_ERRORS = {
    * @property GET_FILTERS_PROPERTIES_ERROR
    * @type Object
    * @final
+   * @default 14
    */
   GET_FILTERS_PROPERTIES_ERROR: {
     code: 0x00e,
@@ -194,6 +208,7 @@ const HTTP_ERRORS = {
    * @property GET_FILTERS_CATEGORIES_ERROR
    * @type Object
    * @final
+   * @default 15
    */
   GET_FILTERS_CATEGORIES_ERROR: {
     code: 0x00f,
@@ -206,6 +221,7 @@ const HTTP_ERRORS = {
    * @property GET_CATEGORIES_ERROR
    * @type Object
    * @final
+   * @default 16
    */
   GET_CATEGORIES_ERROR: {
     code: 0x010,
@@ -218,6 +234,7 @@ const HTTP_ERRORS = {
    * @property STATISTICS_MEDIA_VIEWS_GET_ONE_ERROR
    * @type Object
    * @final
+   * @default 17
    */
   STATISTICS_MEDIA_VIEWS_GET_ONE_ERROR: {
     code: 0x011,
@@ -230,6 +247,7 @@ const HTTP_ERRORS = {
    * @property STATISTICS_MEDIA_VIEWS_INCREMENT_ERROR
    * @type Object
    * @final
+   * @default 18
    */
   STATISTICS_MEDIA_VIEWS_INCREMENT_ERROR: {
     code: 0x012,
@@ -242,6 +260,7 @@ const HTTP_ERRORS = {
    * @property GET_PROMOTED_VIDEOS_GET_SETTINGS_ERROR
    * @type Object
    * @final
+   * @default 19
    */
   GET_PROMOTED_VIDEOS_GET_SETTINGS_ERROR: {
     code: 0x013,
@@ -254,6 +273,7 @@ const HTTP_ERRORS = {
    * @property GET_PROMOTED_VIDEOS_GET_ONE_ERROR
    * @type Object
    * @final
+   * @default 20
    */
   GET_PROMOTED_VIDEOS_GET_ONE_ERROR: {
     code: 0x014,
@@ -266,6 +286,7 @@ const HTTP_ERRORS = {
    * @property GET_PROMOTED_VIDEOS_ERROR
    * @type Object
    * @final
+   * @default 21
    */
   GET_PROMOTED_VIDEOS_ERROR: {
     code: 0x015,
@@ -278,6 +299,7 @@ const HTTP_ERRORS = {
    * @property GET_PROMOTED_VIDEOS_GET_VIDEOS_ERROR
    * @type Object
    * @final
+   * @default 22
    */
   GET_PROMOTED_VIDEOS_GET_VIDEOS_ERROR: {
     code: 0x016,
@@ -290,6 +312,7 @@ const HTTP_ERRORS = {
    * @property GET_PROMOTED_VIDEOS_GET_VIDEO_ERROR
    * @type Object
    * @final
+   * @default 23
    */
   GET_PROMOTED_VIDEOS_GET_VIDEO_ERROR: {
     code: 0x017,
@@ -304,6 +327,7 @@ const HTTP_ERRORS = {
    * @property PATH_NOT_FOUND
    * @type Object
    * @final
+   * @default 256
    */
   PATH_NOT_FOUND: {
     code: 0x100,
@@ -316,6 +340,7 @@ const HTTP_ERRORS = {
    * @property GET_VIDEO_NOT_FOUND
    * @type Object
    * @final
+   * @default 257
    */
   GET_VIDEO_NOT_FOUND: {
     code: 0x101,
@@ -330,6 +355,7 @@ const HTTP_ERRORS = {
    * @property CAS_AUTHENTICATION_FAILED
    * @type Object
    * @final
+   * @default 512
    */
   CAS_AUTHENTICATION_FAILED: {
     code: 0x200,
@@ -342,6 +368,7 @@ const HTTP_ERRORS = {
    * @property BACK_END_EXTERNAL_AUTHENTICATION_FAILED
    * @type Object
    * @final
+   * @default 513
    */
   BACK_END_EXTERNAL_AUTHENTICATION_FAILED: {
     code: 0x201,
@@ -354,6 +381,7 @@ const HTTP_ERRORS = {
    * @property LDAP_AUTHENTICATION_FAILED
    * @type Object
    * @final
+   * @default 514
    */
   LDAP_AUTHENTICATION_FAILED: {
     code: 0x202,
@@ -366,6 +394,7 @@ const HTTP_ERRORS = {
    * @property BACK_END_AUTHENTICATION_FAILED
    * @type Object
    * @final
+   * @default 515
    */
   BACK_END_AUTHENTICATION_FAILED: {
     code: 0x203,
@@ -378,6 +407,7 @@ const HTTP_ERRORS = {
    * @property GET_VIDEO_NOT_ALLOWED
    * @type Object
    * @final
+   * @default 516
    */
   GET_VIDEO_NOT_ALLOWED: {
     code: 0x204,
@@ -390,6 +420,7 @@ const HTTP_ERRORS = {
    * @property BACK_END_UNAUTHORIZED
    * @type Object
    * @final
+   * @default 517
    */
   BACK_END_UNAUTHORIZED: {
     code: 0x205,
@@ -402,6 +433,7 @@ const HTTP_ERRORS = {
    * @property BACK_END_FORBIDDEN
    * @type Object
    * @final
+   * @default 518
    */
   BACK_END_FORBIDDEN: {
     code: 0x206,
@@ -414,6 +446,7 @@ const HTTP_ERRORS = {
    * @property LIVE_FORBIDDEN
    * @type Object
    * @final
+   * @default 519
    */
   LIVE_FORBIDDEN: {
     code: 0x207,
@@ -428,6 +461,7 @@ const HTTP_ERRORS = {
    * @property AUTHENTICATE_EXTERNAL_WRONG_PARAMETERS
    * @type Object
    * @final
+   * @default 768
    */
   AUTHENTICATE_EXTERNAL_WRONG_PARAMETERS: {
     code: 0x300,
@@ -440,6 +474,7 @@ const HTTP_ERRORS = {
    * @property AUTHENTICATE_INTERNAL_WRONG_PARAMETERS
    * @type Object
    * @final
+   * @default 769
    */
   AUTHENTICATE_INTERNAL_WRONG_PARAMETERS: {
     code: 0x301,
@@ -452,6 +487,7 @@ const HTTP_ERRORS = {
    * @property GET_SETTING_MISSING_PARAMETERS
    * @type Object
    * @final
+   * @default 770
    */
   GET_SETTING_MISSING_PARAMETERS: {
     code: 0x302,
@@ -464,6 +500,7 @@ const HTTP_ERRORS = {
    * @property UPDATE_SETTINGS_MISSING_PARAMETERS
    * @type Object
    * @final
+   * @default 771
    */
   UPDATE_SETTINGS_MISSING_PARAMETERS: {
     code: 0x303,
@@ -476,6 +513,7 @@ const HTTP_ERRORS = {
    * @property UPDATE_SETTINGS_WRONG_PARAMETERS
    * @type Object
    * @final
+   * @default 772
    */
   UPDATE_SETTINGS_WRONG_PARAMETERS: {
     code: 0x304,
@@ -488,6 +526,7 @@ const HTTP_ERRORS = {
    * @property SEARCH_WRONG_PARAMETERS
    * @type Object
    * @final
+   * @default 773
    */
   SEARCH_WRONG_PARAMETERS: {
     code: 0x305,
@@ -500,6 +539,7 @@ const HTTP_ERRORS = {
    * @property GET_PROMOTED_VIDEOS_WRONG_PARAMETERS
    * @type Object
    * @final
+   * @default 774
    */
   GET_PROMOTED_VIDEOS_WRONG_PARAMETERS: {
     code: 0x306,
@@ -512,6 +552,7 @@ const HTTP_ERRORS = {
    * @property CONVERT_VIDEO_POI_WRONG_PARAMETERS
    * @type Object
    * @final
+   * @default 775
    */
   CONVERT_VIDEO_POI_WRONG_PARAMETERS: {
     code: 0x307,
@@ -524,6 +565,7 @@ const HTTP_ERRORS = {
    * @property CONVERT_VIDEO_POI_ERROR
    * @type Object
    * @final
+   * @default 776
    */
   CONVERT_VIDEO_POI_ERROR: {
     code: 0x308,
