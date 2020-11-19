976898ae2c9e760cc2d125a5571fb76d43245670
Maxime Béguin <maxime.beguin@veo-labs.com>
2020-11-19 11:02:19 +0100

    Auto-generated documentation for version 7.0.0

---
+++ 7.0.0/api/server/api.js
@@ -0,0 +1,52 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AuthenticationController",
+        "CategoriesController",
+        "DefaultController",
+        "ErrorController",
+        "FiltersController",
+        "GroupsController",
+        "HTTP_ERRORS",
+        "Server",
+        "SettingsController",
+        "StatisticsController",
+        "UserProvider",
+        "VersionController",
+        "VideosController",
+        "authenticator",
+        "context",
+        "openveo-web-service-errors",
+        "statisticsController"
+    ],
+    "modules": [
+        "controllers",
+        "portal",
+        "providers",
+        "storages"
+    ],
+    "allModules": [
+        {
+            "displayName": "controllers",
+            "name": "controllers",
+            "description": "Controllers provide all server route actions."
+        },
+        {
+            "displayName": "portal",
+            "name": "portal",
+            "description": "Defines Portal HTTP Server."
+        },
+        {
+            "displayName": "providers",
+            "name": "providers",
+            "description": "Providers module includes all portal providers."
+        },
+        {
+            "displayName": "storages",
+            "name": "storages",
+            "description": "Include all storages implemented by OpenVeo Portal."
+        }
+    ],
+    "elements": []
+} };
+});
